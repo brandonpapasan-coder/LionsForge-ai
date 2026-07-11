8468884b8cbb5619cc7d853db3dab480185944b5
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-10 21:26:33 -0500

    Mentor: export persistence models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.alert import Alert
 from app.models.company import Company
+from app.models.mentor import MentorConversation, MentorMessage
 from app.models.portfolio import Portfolio, PortfolioHolding
 from app.models.user import User
 from app.models.watchlist import Watchlist
@@ -7,6 +8,8 @@ from app.models.watchlist import Watchlist
 __all__ = [
     "Alert",
     "Company",
+    "MentorConversation",
+    "MentorMessage",
     "Portfolio",
     "PortfolioHolding",
     "User",
