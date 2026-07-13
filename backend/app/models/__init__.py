703b06fc8752f9e71302fd83ccda76ae2d6e7cb9
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-12 19:21:13 -0500

    Entity Resolution: export resolution models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,6 +1,7 @@
 from app.models.alert import Alert
 from app.models.company import Company
 from app.models.education import LessonProgress
+from app.models.entity_resolution import KnowledgeEntityAlias, KnowledgeEntityMergeAudit
 from app.models.knowledge_graph import KnowledgeEntity, KnowledgeRelationship
 from app.models.mentor import MentorConversation, MentorMessage
 from app.models.portfolio import Portfolio, PortfolioHolding
@@ -13,6 +14,8 @@ __all__ = [
     "Alert",
     "Company",
     "KnowledgeEntity",
+    "KnowledgeEntityAlias",
+    "KnowledgeEntityMergeAudit",
     "KnowledgeRelationship",
     "LessonProgress",
     "MentorConversation",
