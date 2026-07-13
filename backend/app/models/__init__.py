eaa8b8382f23c9ea5310001fe7734c24a5fc19c3
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-13 12:49:03 -0500

    Register knowledge memory models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -5,6 +5,7 @@ from app.models.entity_resolution import KnowledgeEntityAlias, KnowledgeEntityMe
 from app.models.evidence import EvidenceRecord
 from app.models.executive_brief_snapshot import ExecutiveBriefSnapshot
 from app.models.knowledge_graph import KnowledgeEntity, KnowledgeRelationship
+from app.models.knowledge_memory import KnowledgeMemory, KnowledgeMemoryRevision
 from app.models.mentor import MentorConversation, MentorMessage
 from app.models.mission import Mission, MissionStep
 from app.models.portfolio import Portfolio, PortfolioHolding
@@ -21,6 +22,8 @@ __all__ = [
     "KnowledgeEntity",
     "KnowledgeEntityAlias",
     "KnowledgeEntityMergeAudit",
+    "KnowledgeMemory",
+    "KnowledgeMemoryRevision",
     "KnowledgeRelationship",
     "LessonProgress",
     "MentorConversation",
