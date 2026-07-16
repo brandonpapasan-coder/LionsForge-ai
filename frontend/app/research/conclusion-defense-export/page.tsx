cfede82a27ef05da0a774e91d15910a18c8f28bd
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-16 16:14:10 -0500

    Add conclusion defense export page

---
+++ frontend/app/research/conclusion-defense-export/page.tsx
@@ -0,0 +1,5 @@
+import { ResearchConclusionDefenseExportPacket } from "@/components/research-conclusion-defense-export-packet";
+
+export default function ResearchConclusionDefenseExportPage() {
+  return <ResearchConclusionDefenseExportPacket />;
+}
