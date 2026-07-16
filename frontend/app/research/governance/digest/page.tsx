f027e0dfb1f89968c26402bdf897d0722db1722b
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-16 10:45:43 -0500

    Add governance digest page

---
+++ frontend/app/research/governance/digest/page.tsx
@@ -0,0 +1,5 @@
+import { ResearchGovernanceDigest } from "@/components/research-governance-digest";
+
+export default function ResearchGovernanceDigestPage() {
+  return <ResearchGovernanceDigest />;
+}
