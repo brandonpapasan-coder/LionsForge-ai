54465a7ec2de97b24664a8ad3a66cdeef25f5c61
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-16 17:36:18 -0500

    Add comparison report page

---
+++ frontend/app/research/packet-comparison-report/page.tsx
@@ -0,0 +1,5 @@
+import { ResearchPacketComparisonReportWorkspace } from "../../../components/research-packet-comparison-report-workspace";
+
+export default function ResearchPacketComparisonReportPage() {
+  return <main><ResearchPacketComparisonReportWorkspace /></main>;
+}
