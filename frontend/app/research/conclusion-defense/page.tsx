a6a4912f0403122346c7799494389e40c3879ea4
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-16 15:31:40 -0500

    Add conclusion defense page

---
+++ frontend/app/research/conclusion-defense/page.tsx
@@ -0,0 +1,5 @@
+import { ResearchConclusionDefenseWorkspace } from "@/components/research-conclusion-defense-workspace";
+
+export default function ResearchConclusionDefensePage() {
+  return <ResearchConclusionDefenseWorkspace />;
+}
