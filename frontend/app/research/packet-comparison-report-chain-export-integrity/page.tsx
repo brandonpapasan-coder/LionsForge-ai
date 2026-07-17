56ac246c173de96a96c1993af544ad3f620ba7dd
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-16 21:52:01 -0500

    Add chain verification report integrity page

---
+++ frontend/app/research/packet-comparison-report-chain-export-integrity/page.tsx
@@ -0,0 +1,9 @@
+import { ResearchPacketComparisonReportChainExportIntegrityChecker } from "../../../components/research-packet-comparison-report-chain-export-integrity-checker";
+
+export default function ResearchPacketComparisonReportChainExportIntegrityPage() {
+  return (
+    <main>
+      <ResearchPacketComparisonReportChainExportIntegrityChecker />
+    </main>
+  );
+}
