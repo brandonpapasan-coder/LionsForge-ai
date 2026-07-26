c8227692a6e80478a08cbe461321bf4ac30b5ac8
brandonpapasan-coder <brandonpapasan@gmail.com>
2026-07-26 09:16:38 -0500

    Add artifact integrity page

---
+++ frontend/app/review-queue/artifact-integrity/page.tsx
@@ -0,0 +1,5 @@
+import { ArtifactIntegrityWorkspace } from "@/components/artifact-integrity-workspace";
+
+export default function ArtifactIntegrityPage() {
+  return <main><ArtifactIntegrityWorkspace /></main>;
+}
